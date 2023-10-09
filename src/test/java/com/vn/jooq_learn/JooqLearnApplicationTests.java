546aa9d354bab210d332bce8895a899e250d3fc4
ismhac <hachac2k2@gmail.com>
2023-10-09 16:19:18 +0700

    # init source

---
+++ src/test/java/com/vn/jooq_learn/JooqLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vn.jooq_learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JooqLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
