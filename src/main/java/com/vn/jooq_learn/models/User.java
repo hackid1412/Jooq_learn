985c59b3ce66ef7c9dbab080e02fff732266906b
ismhac <hachac2k2@gmail.com>
2023-10-09 17:32:08 +0700

    # Update pom.xml for config Jooq

---
+++ src/main/java/com/vn/jooq_learn/models/User.java
@@ -0,0 +1,14 @@
+package com.vn.jooq_learn.models;
+
+import java.util.Date;
+
+public class User {
+    private Long id;
+    private String first_name;
+    private String last_name;
+    private String email;
+    private String password;
+    private Date createdAt;
+    private Date updatedAt;
+    private Date deletedAt;
+}
